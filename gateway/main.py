34d941adf72c779dc3590f304fe8bcb7a88ed0d0
Alex Stoia <alexstoia.as@gmail.com>
2023-10-25 22:29:00 +0200

    Testing code

--- gateway/main.py
+++ gateway/main.py
@@ -1,19 +1,19 @@
 from enum import Enum
 from os import error, getenv
 import paho.mqtt.subscribe as subscribe
-from kafka import KafkaProducer, KafkaConsumer
+#from kafka import KafkaProducer, KafkaConsumer
 
-DEVICE_TYPE={LIGHT, PRESENCE_SENSOR, TEMPERATURE_SENSOR, HEAT_PUMP}
+DEVICE_TYPE={"LIGHT, PRESENCE_SENSOR, TEMPERATURE_SENSOR, HEAT_PUMP"}
 
 def test_print(client, userdata, message):
     print("%s %s" % (message.topic, message.payload))
-    my_producer.send(message.topic, value=message.payload) #Send the information to the kafka broker
+    #my_producer.send(message.topic, value=message.payload) #Send the information to the kafka broker
 
 if __name__ == "__main__":
     print("Starting gateway")
 
-    my_producer = KafkaProducer(bootstrap_servers=['host.docker.internal:9092'], 
-    value_serializer=lambda x: dumps(x).encode('utf-8')) #Connect to the kafka broker
-    _deserializer=lambda x: loads(x.decode('utf-8'))
+    # my_producer = KafkaProducer(bootstrap_servers= getenv("HOSTNAME"), 
+    # value_serializer=lambda x: dumps(x).encode('utf-8')) #Connect to the kafka broker
+    # _deserializer=lambda x: loads(x.decode('utf-8'))
 
-    subscribe.callback(test_print, "+", hostname="host.docker.internal") #Subscribe to the mqtt broker
+    subscribe.callback(test_print, ["+"], hostname= str(getenv("MQTT_HOSTNAME")) ) #Subscribe to the mqtt broker
