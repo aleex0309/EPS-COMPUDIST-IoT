9e3705bbe8f8df493c6e8a591b3d579d9bbd4dff
marcgj <mgaspjoval2002@gmail.com>
2023-11-16 18:12:50 +0100

    idk

--- microservices/cleanup/main.py
+++ microservices/cleanup/main.py
@@ -1,5 +1,7 @@
+from operator import truediv
 from os import getenv
 from json import loads
+from time import sleep
 
 from kafka import KafkaConsumer
 
@@ -12,11 +14,25 @@ def deserializer(value: bytes):
 if __name__ == "__main__":
     print("Starting CLEANUP SERVICE...")
 
-    consumer = KafkaConsumer(
-        "CLEAN",
-        bootstrap_servers=[str(getenv("KAFKA_BROKER_HOSTNAME"))],
-        value_deserializer=deserializer,
-    )
+    hostname = getenv("KAFKA_BROKER_HOSTNAME")
+    while True:
+        try:
+            print(f"Trying to connect to broker {hostname}...", flush=True)
+            consumer = KafkaConsumer(
+                "CLEAN",
+                bootstrap_servers=[str(getenv("KAFKA_BROKER_HOSTNAME"))],
+                value_deserializer=deserializer,
+            )
+
+            if consumer.bootstrap_connected():
+                break
+
+        except Exception:
+            pass
+
+        sleep(1)
+    if consumer.bootstrap_connected():
+        print("Connection Established!")
 
     for msg in consumer:
         print(msg.value)
